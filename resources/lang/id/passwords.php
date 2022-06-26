64455c657a918427f5a2af8f673ea01deeb402db
Annas Abdurrahman <annasabdurrahman354@gmail.com>
2022-06-26 17:49:41 +0700

    change

--- resources/lang/id/passwords.php
+++ resources/lang/id/passwords.php
@@ -3,7 +3,7 @@
 return [
     'password' => 'Kata sandi minimal harus memiliki enam karakter dan cocok dengan konfirmasi.',
     'reset'    => 'Kata sandi anda sudah direset!',
-    'sent'     => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
+    'sent'     => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda! Hubungi sie keilmuan jika tidak mendapat email!',
     'token'    => 'Token pengaturan ulang kata sandi tidak sah.',
     'user'     => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
     'updated'  => 'Password anda sudah di ubah',
