814602d87a94572cd4630ec424299cc70a86b44a
Annas Abdurrahman <annasabdurrahman354@gmail.com>
2022-07-04 15:43:02 +0700

    closing User Munaqosah Taking Plot

--- resources/views/livewire/user/munaqosah/plot.blade.php
+++ resources/views/livewire/user/munaqosah/plot.blade.php
@@ -156,7 +156,7 @@
 
     <div class="card-body">
         <div class="pt-3">
-            @if(!$taken && !$full && !$lewat && !$tabrakan && $angkatan)
+            @if(!$taken && !$full && !$lewat && !$tabrakan && $angkatan && false)
             <div class="text-center mt-6">
                 <button wire:click="ambilJadwal" class="md:w-1/4 bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                     Ambil Jadwal Munaqosah
